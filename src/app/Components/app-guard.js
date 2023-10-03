5fbe4d4eba727ab1a8f37f27a673e33b185b1d65
LocTM <tranminhloc.anhk@gmail.com>
2023-10-03 17:18:14 +0700

    ok

--- src/app/Components/app-guard.js
+++ src/app/Components/app-guard.js
@@ -3,6 +3,7 @@
 import { useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
 import { AppButton } from "./app-button";
+import { Labrada } from "next/font/google";
 
 export const AppGuard = ({ children }) => {
     const router = useRouter();
@@ -14,12 +15,16 @@ export const AppGuard = ({ children }) => {
         return (
             <>
               <div className= "min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
-                 <div>
+                 <div className="pt-10 text-red-800 text-center">
+                    
                 Authentication required. Please sign in!
+                  
                 </div>
-                <AppButton className=" px-4 py-2 text-sm rounded-full cursor-pointer mt-4 text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" onClick={signIn}>
+                <div className="flex items-center justify-center"  >
+                <AppButton className="mt-4 text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" onClick={signIn}>
                     Sign in
-                </AppButton>     
+                </AppButton>    
+                </div> 
               </div>   
             </>
         );
