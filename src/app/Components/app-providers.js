710948bbe51ea438986ca6a7fc70f22948a7d03e
LocTM <tranminhloc.anhk@gmail.com>
2023-10-03 15:40:21 +0700

    ok

--- src/app/Components/app-providers.js
+++ src/app/Components/app-providers.js
@@ -24,9 +24,11 @@ export const AppProvider = ({ children }) => {
         <Provider store={store}>
             <AppFirebase>
         <ThemeContext.Provider value={theme}>
+            <div className="w-full bg-blue-700">
             <AppButton className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800" color="blue" onClick={toggleTheme}>
                 Toggle theme
             </AppButton>
+            </div>
             {children}
         </ThemeContext.Provider>
         </AppFirebase>
