3c15854ef57015d75476e98f30339f7719561971
LocTM <tranminhloc.anhk@gmail.com>
2023-10-03 17:00:52 +0700

    ok

--- src/app/Components/app-guard.js
+++ src/app/Components/app-guard.js
@@ -15,9 +15,9 @@ export const AppGuard = ({ children }) => {
             <>
               <div className= "min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
                  <div>
-                    Authentication required. Please sign in
+                Authentication required. Please sign in!
                 </div>
-                <AppButton className=" text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" onClick={signIn}>
+                <AppButton className=" px-4 py-2 text-sm rounded-full cursor-pointer mt-4 text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" onClick={signIn}>
                     Sign in
                 </AppButton>     
               </div>   
